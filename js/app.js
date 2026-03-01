2af16c0f99b9bd0e194a58d19421957676fc189e
Thantzinaung94 <thantzinaung2025.94@gmail.com>
2026-03-01 18:50:16 +0630

    A expenses & revenue resources

--- js/app.js
+++ js/app.js
@@ -9,3 +9,27 @@ for(var i = 0; i < getcategories.length; i++)
         this.classList.add('current');
     });
 };
+
+
+// Start Revenue Sources
+
+google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Return Item',     11],
+          ['Direct Sales',      2],
+          ['Online Sales',  2],
+        ]);
+
+        var options = {
+        //   title: 'My Daily Activities',
+          pieHole: 0.4,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+      }
+
+// End Revenue Sources
